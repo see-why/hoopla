08ada9f1e3462759d52882e8b01b356e3da8e7c8
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-11-29 23:53:03 -0500

    Add initial implementation of Hybrid Search CLI

---
+++ cli/hybrid_search_cli.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Hybrid Search CLI")
+    parser.add_subparsers(dest="command", help="Available commands")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
